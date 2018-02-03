9c568478522b248e833070a11359d326c4e1cd00
puzcoin <akuma@mud.com.cn>
2018-02-03 10:22:36 -0500

    f

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -70,7 +70,7 @@ static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesi
 
 static CBlock CreateGenesisBlock(uint64_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
-	const char* pszTimestamp = "The Guardian: [2nd Feb 2017] Finsbury Park mosque wins apology and damages from Thomson Reuters";
+	const char* pszTimestamp = "The SuperCellCoin was started at 2018/2/3,based on a new idea of super-cell masternodes.";
     const CScript genesisOutputScript = CScript() << ParseHex("045622582bdfad9366cdff9652d35a562af17ea4e3462d32cd988b32919ba2ff4bc806485be5228185ad3f75445039b6e744819c4a63304277ca8d20c99a6acec8") << OP_CHECKSIG;
     return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, nVersion, genesisReward);
 }
@@ -87,17 +87,30 @@ public:
         pchMessageStart[3] = 0xc8;
         vAlertPubKey = ParseHex("040fd972dba056779d9f998cba8d5866e47fb875fd8cb9c4d36baf88db738a6ffbc581e0fad7f2f129c7f814d81baeda567a3735aaf0bfbc339f40359d4a52b4bf");
         nDefaultPort = DEFAULT_PORT;
-        nRPCPort = DEFAULT_PORT;
+        nRPCPort = DEFAULT_RPCPORT;
         nProofOfWorkLimit = uint256S("000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         nProofOfStakeLimit = uint256S("00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         nTargetSpacing = TARGET_SPACKING;
         nTargetTimespan = 7 * 24 * 60 * 60; // one week
 
-	genesis = CreateGenesisBlock(1486045800, 28884498, 0x1e00ffff, 1, (1 * COIN));
+	genesis = CreateGenesisBlock(GENESIS_BLOCK_TIME, 224490, 0x1e00ffff, 1, (0 * COIN));
 	hashGenesisBlock = genesis.GetHash();
+/*
+	for(int ii = 0 ;ii<1000000;ii++) {
+		genesis = CreateGenesisBlock(GENESIS_BLOCK_TIME, ii, 0x1e00ffff,1, 0 * COIN);
+		printf("nNonce = %9u,hash=",ii);
+		const std::string &str = genesis.GetHash().GetHex();
+		fwrite(str.data(), 1, str.size(), stdout);
+		printf(",root=");
+		const std::string &str2 = genesis.hashMerkleRoot.GetHex();
+		fwrite(str2.data(), 1, str2.size(), stdout);
+		printf("\n");
+	}
+*/
+//
 	
-        assert(hashGenesisBlock == uint256("0x0000004cf5ffbf2e31a9aa07c86298efb01a30b8911b80af7473d1114715084b"));
-        assert(genesis.hashMerkleRoot == uint256("0x7af2e961c5262cb0411edcb7414ab7178133fc06257ceb47d349e4e5e35e2d40"));
+        assert(hashGenesisBlock == uint256("0x0000070cef98194093249b1571925396d8fe5944498e669540cb40f8aa9ea190"));
+        assert(genesis.hashMerkleRoot == uint256("0xcc196acbef54cee12253a4247c76e28291fe2f3c81ae5535a01013b792bae203"));
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,125);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,90);
@@ -161,8 +174,8 @@ public:
         
 	hashGenesisBlock = genesis.GetHash();
 	
-	assert(hashGenesisBlock == uint256("0x0000002bed128b6b2a62bd8edd4e6f8a414eac38e256aa0194adb8c93fe18132"));		
-	assert(genesis.hashMerkleRoot == uint256("0x24c7b9bada3984f99ec9072672d3f7180af711b5eed5b82b321848b973b4a41e"));
+	//assert(hashGenesisBlock == uint256("0x0000002bed128b6b2a62bd8edd4e6f8a414eac38e256aa0194adb8c93fe18132"));		
+	//assert(genesis.hashMerkleRoot == uint256("0x24c7b9bada3984f99ec9072672d3f7180af711b5eed5b82b321848b973b4a41e"));
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,97);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196);
